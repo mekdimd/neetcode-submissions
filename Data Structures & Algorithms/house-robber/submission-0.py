a0bad28bb57311b2cd6cd9f0351d56c41991cb14
Mekdim Dereje <96852675+mekdimd@users.noreply.github.com>
2026-04-08 11:52:33 -0700

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.py
@@ -0,0 +1,63 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # return self.rob_top_down(nums)
+        return self.rob_bottom_up_const(nums)
+    
+    
+    def rob_top_down(self, nums: List[int]) -> int:
+        memo = {}
+        def dfs(i):
+            # out of bounds
+            if i < 0 or i >= len(nums):
+                return 0
+
+            # max value for house
+            if i in memo:
+                return memo[i]
+                
+            # rob current house or neighbour
+            memo[i] = max(nums[i] + dfs(i+2), dfs(i+1))
+            return memo[i]
+
+        return dfs(0)
+
+
+    def rob_bottom_up(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        n = len(nums)
+        memo = [0] * n
+
+        for i in range(n-2):
+            memo[i] = max(nums[i] + nums[i+2], nums[i+1])
+
+        return memo[-1]
+
+
+    def rob_bottom_up_const(self, nums: List[int]) -> int:
+        """
+        [rob1, rob2, n, n+1, ...]
+                     ^ next house
+        - either dont rob next house (max val is rob2)
+        - or do curr house + prev neighbour (n + rob1)
+
+        O(n) time
+        O(1) space
+        """
+        if not nums:
+            return 0
+        
+        # rob first house
+        if len(nums) == 1:
+            return nums[0]
+        
+        if len(nums) == 2:
+            return max(nums[0], nums[1])
+    
+        rob1, rob2 = 0, 0
+
+        for n in nums:
+            rob1, rob2 = rob2, max(rob2, n + rob1)
+
+        return rob2
